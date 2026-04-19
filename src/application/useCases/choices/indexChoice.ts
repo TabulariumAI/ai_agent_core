6d05d3b52721cc4431ac67619a82d3b0a0e45c75
Vardan <harvart@gmail.com>
2026-04-18 20:10:54 -0700

    Update service names in indexChoice

--- src/application/useCases/choices/indexChoice.ts
+++ src/application/useCases/choices/indexChoice.ts
@@ -3,32 +3,32 @@ import * as Entities from "../../../core/entities/imports";
 export const indexChoice: Entities.Choice = {
     items: [
         {
-            service: "Recognition",
-            level: 5
+            "service": "Recognition",
+            "level": 5
         },
         {
-            service: "TransactionIndexing",
-            level: 1
+            "service": "PartyClauseIndexing",
+            "level": 1
         },
         {
-            service: "PartyClauseIndexing",
-            level: 1
+            "service": "RecitalIndexing",
+            "level": 1
         },
         {
-            service: "ExhibitIndexing",
-            level: 1
+            "service": "ExhibitIndexing",
+            "level": 1
         },
         {
-            service: "RecitalIndexing",
-            level: 1
+            "service": "LegalEnrichment",
+            "level": 1
         },
         {
-            service: "LegalEnrichment",
-            level: 1
+            "service": "PartyEnrichment",
+            "level": 1
         },
         {
-            service: "PartyEnrichment",
-            level: 1
+            "service": "Validation",
+            "level": 1
         }
     ]
 }
