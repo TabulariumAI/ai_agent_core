85b8a9b1bea186ea6532bbd33e183914aeb98fa6
Vardan <harvart@gmail.com>
2026-04-18 20:06:35 -0700

    Change level of 'Recognition' service to 6
    Updated the level of the 'Recognition' service from 3 to 6.

--- src/application/useCases/choices/provisionChoice.ts
+++ src/application/useCases/choices/provisionChoice.ts
@@ -3,8 +3,48 @@ import * as Entities from "../../../core/entities/imports";
 export const provisionChoice: Entities.Choice = {
     items: [
         {
-            service: 'Recognition',
-            level: 3
-        }
+            "service": "Recognition",
+            "level": 6
+        },
+        {
+            "service": "ConfidentialIndexing",
+            "level": 1
+        },
+        {
+            "service": "TransactionIndexing",
+            "level": 1
+        },
+        {
+            "service": "EndorsementIndexing",
+            "level": 1
+        },
+        {
+            "service": "PartyClauseIndexing",
+            "level": 1
+        },
+        {
+            "service": "RecitalIndexing",
+            "level": 1
+        },
+        {
+            "service": "ExhibitIndexing",
+            "level": 1
+        },
+        {
+            "service": "MonetaryInfoIndexing",
+            "level": 1
+        },
+        {
+            "service": "AcknowledgmentIndexing",
+            "level": 1
+        },
+        {
+            "service": "LegalEnrichment",
+            "level": 1
+        },
+        {
+            "service": "PartyEnrichment",
+            "level": 1
+        },
     ]
 }
