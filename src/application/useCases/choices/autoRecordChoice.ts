ff1bbd3212490cca4f00bb5fb1a72ec8a47c694e
Vardan <harvart@gmail.com>
2026-04-18 20:12:14 -0700

    Change service level for Recognition to 6
    Updated the service levels in the autoRecordChoice configuration.

--- src/application/useCases/choices/autoRecordChoice.ts
+++ src/application/useCases/choices/autoRecordChoice.ts
@@ -3,8 +3,52 @@ import * as Entities from "../../../core/entities/imports";
 export const autoRecordChoice: Entities.Choice = {
     items: [
         {
-            service: 'Recognition',
-            level: 3
+            "service": "Recognition",
+            "level": 6
+        },
+        {
+            "service": "ConfidentialIndexing",
+            "level": 1
+        },
+        {
+            "service": "TransactionIndexing",
+            "level": 1
+        },
+        {
+            "service": "EndorsementIndexing",
+            "level": 1
+        },
+        {
+            "service": "PartyClauseIndexing",
+            "level": 1
+        },
+        {
+            "service": "RecitalIndexing",
+            "level": 1
+        },
+        {
+            "service": "ExhibitIndexing",
+            "level": 1
+        },
+        {
+            "service": "MonetaryInfoIndexing",
+            "level": 1
+        },
+        {
+            "service": "AcknowledgmentIndexing",
+            "level": 1
+        },
+        {
+            "service": "LegalEnrichment",
+            "level": 1
+        },
+        {
+            "service": "PartyEnrichment",
+            "level": 1
+        },
+        {
+            "service": "Validation",
+            "level": 1
         }
     ]
 }
